e3fb5dcd6077e2d2c231fb617517a6246eb4c8ec
AsotoV22 <alexsotov97@gmail.com>
2024-03-15 01:20:45 -0600

    Completar la seccion pruebas funcional

--- src/main/java/com/proyecto/controller/PruebasController.java
+++ src/main/java/com/proyecto/controller/PruebasController.java
@@ -4,7 +4,7 @@
  */
 package com.proyecto.controller;
 
-
+import com.proyecto.domain.Categoria;
 import com.proyecto.service.CategoriaService;
 import com.proyecto.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,20 +20,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/pruebas")
 public class PruebasController {
-    
+
     @Autowired
     private ProductoService productoService;
     @Autowired
     private CategoriaService categoriaService;
-    
+
     @GetMapping("/listado")
-    public String listado(Model model){
-        
+    public String listado(Model model) {
+
         var lista = productoService.getProductos(false);
-        
+
         model.addAttribute("productos", lista);
         var categorias = categoriaService.getCategorias(false);
-        
+
+        model.addAttribute("categorias", categorias);
+        return "/pruebas/listado";
+    }
+
+    @GetMapping("/listado/{idCategoria}")
+    public String modifica(Categoria categoria, Model model) {
+        categoria = categoriaService.getCategoria(categoria);
+        var lista = categoria.getProductos();
+        model.addAttribute("productos", lista);
+        var categorias = categoriaService.getCategorias(false);
         model.addAttribute("categorias", categorias);
         return "/pruebas/listado";
     }
