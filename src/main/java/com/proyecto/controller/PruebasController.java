9b4faf788ea3d6a7dc6d498711124152d82f805e
AsotoV22 <alexsotov97@gmail.com>
2024-03-15 00:20:48 -0600

    Creacion de Mapping/Pruebas

---
+++ src/main/java/com/proyecto/controller/PruebasController.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyecto.controller;
+
+
+import com.proyecto.service.CategoriaService;
+import com.proyecto.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author esanarru
+ */
+@Controller
+@RequestMapping("/pruebas")
+public class PruebasController {
+    
+    @Autowired
+    private ProductoService productoService;
+    @Autowired
+    private CategoriaService categoriaService;
+    
+    @GetMapping("/listado")
+    public String listado(Model model){
+        
+        var lista = productoService.getProductos(false);
+        
+        model.addAttribute("productos", lista);
+        var categorias = categoriaService.getCategorias(false);
+        
+        model.addAttribute("categorias", categorias);
+        return "/pruebas/listado";
+    }
+}
